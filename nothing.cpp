c6a85d48d75833b4001ebc7c1280447fc18f5a80
haideeyuan <32299609+haideeyuan@users.noreply.github.com>
2022-03-26 14:13:02 +0800

    Update nothing.cpp

--- nothing.cpp
+++ nothing.cpp
@@ -1,2 +1,3 @@
-MPI_Allreduce(biasCountsIn.data(), biasCounts.data(), 
-              numSimulations, MPI_INT, MPI_SUM, simulationMastersComm);
+MPI_Allreduce(biasCountsIn.data(), 
+              biasCounts.data(), numSimulations, 
+              MPI_INT, MPI_SUM, simulationMastersComm);
