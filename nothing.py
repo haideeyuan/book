e86d95cc6f22a06b86ec48fb4486ec42e2da4ed5
haideeyuan <32299609+haideeyuan@users.noreply.github.com>
2022-03-26 14:58:04 +0800

    Update nothing.py

--- nothing.py
+++ nothing.py
@@ -15,3 +15,35 @@ W_fc1 = weight_variable([7*7*64, 1024])
 b_fc1 = bias_variable([1024])
 ## Dropout层 ##
 h_fc1_drop = tf.nn.dropout(b_fc1, keep_prob)
+
+
+
+
+
+
+# Build the Complex
+cmd = "gmx editconf -f jz4_ini.pdb -o jz4.gro"
+os.system(cmd)
+cmd = "%s merge_complex_gro.py -t 3HTB_processed.gro -l jz4.gro" % py
+os.system(cmd)
+cmd = "%s topol_modify.py -l jz4" % py
+os.system(cmd)
+
+# Defining the Unit Cell & Adding Solvent
+cmd = "gmx editconf -f complex.gro -o newbox.gro -bt dodecahedron -d 1.0 " + box
+os.system(cmd)
+cmd = "gmx solvate -cp newbox.gro -cs spc216.gro -p topol.top -o solv.gro"
+os.system(cmd)
+# Adding Ions
+cmd = "gmx grompp -f ions.mdp -c solv.gro -p topol.top -o ions.tpr"
+os.system(cmd)
+cmd = "echo '15\n'|gmx genion -s ions.tpr -o solv_ions.gro 
+            -p topol.top -pname NA -nname CL -neutral"
+os.system(cmd)
+
+# Energy Minimization
+cmd = "gmx grompp -f em.mdp -c solv_ions.gro -p topol.top -o em.tpr"
+os.system(cmd)
+cmd = "gmx mdrun -v -deffnm em"
+os.system(cmd)
+……
